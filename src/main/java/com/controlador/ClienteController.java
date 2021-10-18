21467b2e4e6e901d1c8f735ad3e8e292882b251a
Jessica Vanegas <vanegas.pazzo.jessica@gmail.com>
2021-10-18 12:23:00 -0500

    Hojas html y union con backend Se realiza autorización de CORS, para que se puedan consultar los servicios desde cualquier html NOTA: el html de lista de usuarios y de clientes hay que moverlo a reportes.

--- src/main/java/com/controlador/ClienteController.java
+++ src/main/java/com/controlador/ClienteController.java
@@ -4,18 +4,13 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.modelo.Cliente;
 import com.services.ClienteService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE})
 @RequestMapping("/clientes")
 public class ClienteController {
 	@Autowired
--- src/main/java/com/controlador/VentaController.java
+++ src/main/java/com/controlador/VentaController.java
@@ -4,18 +4,13 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.modelo.Venta;
 import com.services.VentaService;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE})
 @RequestMapping("/ventas")
 public class VentaController {
 	@Autowired
